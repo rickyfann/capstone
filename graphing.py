f86e532f7e1130f9632b39b44a64511935077e52
rickyfann3265 <ricky-fan-617@live.ca>
2023-11-28 18:52:30 -0500

    transitioned to plotly for plotting

--- graphing.py
+++ graphing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 # STEAM OUTLET TEMP APPROX 377.4 F
 
@@ -17,7 +17,6 @@ def grab_year_indices(year, stamps):
     
     return(start_index, final_index)
 
-# data = pd.read_excel(r'C:\Users\SticksandStones\OneDrive\Documents\CAPSTONE\Raw Data.xlsx')
 data = pd.read_excel(r'Raw Data.xlsx')
 
 stamp_values = np.array(data.iloc[:,0], dtype=str)
@@ -38,7 +37,6 @@ d = dict({
 })
 
 collection = pd.DataFrame(data=d)
-# frame = pd.DataFrame(data=d)
 
 frame = []
 
@@ -69,10 +67,6 @@ years = [
     2023
 ]
 
-# fig, ax = plt.subplots(1, figsize=[10,7])
-
-fig,ax = plt.subplots(2,1, figsize=[10,15])
-
 time = np.array(frame['Time'])
 T1 = np.array(frame['Temperature 1'])
 T2 = np.array(frame['Temperature 2'])
@@ -86,68 +80,81 @@ p1 = np.poly1d(z1)
 z2 = np.polyfit(time, de, 1)
 p2 = np.poly1d(z2)
 
-# ax.plot(time, T1, '.', label="Temperature In")
-# ax.plot(time, T2, '.', label="Temperature Out")
-# ax.plot(time, p1(time), label="Trendline")
-# ax.set_title("Temperature vs Time (2018-2023)")
-# ax.set_ylabel("Temperature (deg C)")
-# ax.set_xlabel("Time (h)")
-# ax.legend()
+for (i, (ix,year)) in enumerate(zip(indices, years)):
 
-ax[0].plot(time, T1, '.', label="Temperature In")
-ax[0].plot(time, T2, '.', label="Temperature Out")
-ax[0].plot(time, p1(time), label="Trendline")
+    z1 = np.polyfit(time[ix[0]:ix[1]], T2[ix[0]:ix[1]], 1)
+    p1 = np.poly1d(z1)
 
-# ax[1].plot(time, de, '.', label="Temperature Difference")
-# ax[1].plot(time, p2(time), label="Trendline")
-# ax[1].set_ylim([0,160])
+    z2 = np.polyfit(time[ix[0]:ix[1]], de[ix[0]:ix[1]], 1)
+    p2 = np.poly1d(z2)
 
-ax[1].plot(time, f, '.', label="Correction Factor")
+    fig = make_subplots(rows=1, cols=3,
+                        subplot_titles=(f"Temperature vs Time ({year})", f"Temperature Difference vs Time ({year})", f"Correction Factor vs Time ({year})"))
+    fig.add_trace(
+        go.Scatter(
+            x = time[ix[0]:ix[1]],
+            y = T1[ix[0]:ix[1]],
+            mode = "markers",
+            name = "Temperature In",
+            ),
+        row=1, col=1,
+        )
+    
+    fig.add_trace(
+        go.Scatter(
+            x = time[ix[0]:ix[1]],
+            y = T2[ix[0]:ix[1]],
+            mode="markers",
+            name = "Temperature Out",
+            ),
+        row=1, col=1,
+        )
+    
+    fig.add_trace(
+        go.Scatter(
+            x = time[ix[0]:ix[1]],
+            y = p1(time[ix[0]:ix[1]]),
+            name = "Trendline",
+            ),
+        row=1, col=1,
+        )
+    
+    fig.add_trace(
+        go.Scatter(
+            x = time[ix[0]:ix[1]],
+            y = de[ix[0]:ix[1]],
+            mode = "markers",
+            name = "Temperature Delta",
+            ),
+        row=1, col=2,
+        )
+    
+    fig.add_trace(
+        go.Scatter(
+            x = time[ix[0]:ix[1]],
+            y = p2(time[ix[0]:ix[1]]),
+            name = "Trendline",
+            ),
+        row=1, col=2,
+        )
+    
+    fig.add_trace(
+        go.Scatter(
+            x = time[ix[0]:ix[1]],
+            y = f[ix[0]:ix[1]],
+            mode = "markers",
+            name = "Correction Factor",
+            ),
+        row=1, col=3,
+        )
+    
+    fig['layout']['xaxis']['title'] = "Time (h)"
+    fig['layout']['xaxis2']['title'] = "Time (h)"
+    fig['layout']['xaxis3']['title'] = "Time (h)"
+    fig['layout']['yaxis']['title'] = "Temperature (C)"
+    fig['layout']['yaxis2']['title'] = "Temperature (C)"
+    fig['layout']['yaxis3']['title'] = "Correction Factor"
+    fig['layout']['yaxis']['range'] = (0, 350)
+    fig['layout']['yaxis2']['range'] = (0, 350)
 
-for axis in ax:
-    axis.set_title("Temperature vs Time (2018-2023)")
-    axis.set_ylabel("Temperature (deg C)")
-    axis.set_xlabel("Time (h)")
-    axis.legend()
-
-# ax[1].set_title("Temperature Difference vs Time (2018-2023)")
-
-ax[1].set_title("Correction Factor vs Time (2018-2023)")
-ax[1].set_ylabel("Correction Factor")
-
-# for (i, (ix,year)) in enumerate(zip(indices, years)):
-#     fig,ax = plt.subplots(1,3, figsize=[30,7])
-
-#     z1 = np.polyfit(time[ix[0]:ix[1]], T2[ix[0]:ix[1]], 1)
-#     p1 = np.poly1d(z1)
-
-#     z2 = np.polyfit(time[ix[0]:ix[1]], de[ix[0]:ix[1]], 1)
-#     p2 = np.poly1d(z2)
-
-#     ax[0].plot(time[ix[0]:ix[1]], T1[ix[0]:ix[1]], '.',label="T In")
-#     ax[0].plot(time[ix[0]:ix[1]], T2[ix[0]:ix[1]], '.',label="T Out")
-#     ax[1].plot(time[ix[0]:ix[1]], de[ix[0]:ix[1]], '.',label="T delta")
-#     ax[2].plot(time[ix[0]:ix[1]], f[ix[0]:ix[1]], '.',label="Correction Factor")
-#     ax[0].plot(time[ix[0]:ix[1]], p1(time[ix[0]:ix[1]]), label="Trendline")
-#     ax[1].plot(time[ix[0]:ix[1]], p2(time[ix[0]:ix[1]]), label="Trendline")
-
-#     ax[0].set_title(f"Temperature vs Time ({year})")
-#     ax[0].set_xlabel("Time(h)")
-#     ax[0].set_ylabel("Temperature (deg C)")
-#     ax[0].legend()
-
-#     ax[1].set_title(f"Temperature Difference vs Time ({year})")
-#     ax[1].set_xlabel("Time(h)")
-#     ax[1].set_ylabel("Temperature (deg C)")
-#     ax[1].set_ylim([0,160])
-
-#     ax[2].set_title(f"Correction Factor vs Time ({year})")
-#     ax[2].set_xlabel("Time(h)")
-#     ax[2].set_ylabel("Correction Factor")
-
-# diffs = frame['Delta'].diff()
-# diffs = diffs.where(diffs<20)
-# diffs = diffs.where(diffs>-20)
-
-# plt.figure()
-# plt.plot(time, np.array(diffs))
+    fig.show()
