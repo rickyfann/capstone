9c7b9e30ddeb72f70e6fc676e399e691ec3da588
rickyfann3265 <ricky-fan-617@live.ca>
2024-03-09 14:22:53 -0500

    data smoothing and highlithing of areas of interest

--- prophet_testing.py
+++ prophet_testing.py
@@ -4,6 +4,19 @@ import matplotlib.pyplot as plt
 from prophet import Prophet
 from prophet.plot import add_changepoints_to_plot
 
+def smoothTriangle(data, degree):
+    triangle=np.concatenate((np.arange(degree + 1), np.arange(degree)[::-1])) # up then down
+    smoothed=[]
+
+    for i in range(degree, len(data) - degree * 2):
+        point=data[i:i + len(triangle)] * triangle
+        smoothed.append(np.sum(point)/np.sum(triangle))
+    # Handle boundaries
+    smoothed=[smoothed[0]]*int(degree + degree/2) + smoothed
+    while len(smoothed) < len(data):
+        smoothed.append(smoothed[-1])
+    return smoothed
+
 # reads file
 data = pd.read_excel(r'Raw Data.xlsx')
 
@@ -42,6 +55,7 @@ for i, (val1, val2, val3, val4, val5) in enumerate(zip(collection['Temperature 1
                                            )
                                            ):
     
+    # if True:
     if 160<=val1<=215 and 270<=val2<=380 and 90<=val3<=160 and val4<=1 and val5>0:
         frame.append(collection.iloc[i,:])
 
@@ -52,7 +66,8 @@ frame = pd.DataFrame(frame)
 a = pd.DataFrame(
     {
     'ds': frame.iloc[:,0],
-    'y': frame.iloc[:,6]
+    'y': frame.iloc[:, 6]
+    # 'y': smoothTriangle(frame.iloc[:,6], 6)
     }
     )
 
@@ -94,25 +109,44 @@ future['cap'] = 1
 
 # grabs the predicted values
 forecast = m.predict(future)
+plt.rcParams.update({'font.size': 22})
 
 # plots the prediction alongside original data points
-fig1 = m.plot(forecast, xlabel="Time", ylabel="Correction Factor", include_legend=True, figsize=(10,6))
-
+fig1 = m.plot(forecast, xlabel="Time", ylabel="Correction Factor", include_legend=True, figsize=(15,9))
 # plots change points as vertical red lines
 b = add_changepoints_to_plot(fig1.gca(), m, forecast)
+
+# plt.axvspan(20000, 20100, alpha=0.5)
+plt.axvspan(18110, 18260, color="b", alpha=0.5)
+plt.axvspan(18500, 18755, color="b", alpha=0.5)
+# plt.plot(forecast["ds"][forecast["ds"]<"2022"][forecast["ds"]>"2020"], forecast["trend_lower"][forecast["ds"]<"2022"][forecast["ds"]>"2020"], color="green")
+# plt.plot(np.arange(18100, 18500), forecast["trend_lower"][18100:18500], color="green")
+# upper bound is 20000, lower bound is like 17500 or something
 
 # graph formatting
 plt.title(f"Prophet Prediction - Scale:{changepoint_scale}")
 plt.legend()
+plt.savefig("prediction_graph.jpg")
 
 # plots heat duty on twin axis
-fig2, ax = plt.subplots(figsize = [10,6])
-ax.plot(frame["Stamp"], frame["Heat Duty"], color='green', label="Heat Duty")
+fig2, ax = plt.subplots(figsize = [15,9])
+ax.plot(frame["Stamp"], smoothTriangle(frame["Heat Duty"], 50), color='green', label="Heat Duty")
+plt.axvspan(18110, 18260, color="b", alpha=0.5)
+plt.axvspan(18500, 18755, color="b", alpha=0.5)
 ax.set_title("Heat Duty vs Time")
 ax.set_ylabel("Heat Duty (kW)")
 ax.set_xlabel("Time")
 ax.legend()
 
-# ax2 = plt.twinx()
+# ax[1].plot(forecast["trend_lower"][forecast["ds"]>"2022"][forecast["ds"]>"2020"])
+
+# ax2 = plt.twinx()//
 # ax2.plot(frame["Stamp"], frame["Heat Duty"], color='green', label="Heat Duty")
 # ax2.set_ylabel("Heat Duty (kW)")
+#costing function
+# C = lambda x : (180e3 / 5 / 6 + 200e3 / 6) * x
+
+# duty_data = smoothTriangle(frame["Heat Duty"], 50)
+plt.savefig("heat_duty_graph.jpg", transparent=False)
+
+# fig3 = plt.plot(forecast["trend_lower"][forecast["ds"]>"2022"][forecast["ds"]>"2020"], xlabel="Time", ylabel="Correction Factor", include_legend=True, figsize=(10,6))
